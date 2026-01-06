8b591509e35d6c76a4762cb20ec5a53764b16512
Bhargavi <bhaavi1207@gmail.com>
2026-01-06 14:50:48 +0530

    Add songs data to songs.js

---
+++ src/data/songs.js
@@ -0,0 +1,7 @@
+const songs = [
+  { id: 1, title: "Good 4 u ", artist: "Olivia Rodrigo" },
+  { id: 2, title: "Shape of You", artist: "Ed Sheeran" },
+  { id: 3, title: "Levitating", artist: "Dua Lipa" }
+];
+
+export default songs;
